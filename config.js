7c783655c3fb61b10c2ad9a6e9c2996e69a7012f
Keshav Reddy <keshavlingala@gmail.com>
2021-12-22 23:47:46 +0530

    confidentials removed from config

---
+++ config.js
@@ -0,0 +1,16 @@
+import ffmpeg from 'ffmpeg-cli'
+export const image_base = "https://image.tmdb.org/t/p/w500/";
+export const apiToken = "*******";
+export const tmdb_api = "*******";
+export const bot_base_url = "https://api.telegram.org/bot";
+export const tmdb_base = "https://api.themoviedb.org/3";
+export const movie_search_base = tmdb_base + "/search/movie";
+export const moviedetails = tmdb_base + "/movie/";
+export const youtube_api = "*******";
+export const ffmpegPath = ffmpeg.path;
+export const DIALOGFLOW_PRIVATE_KEY = '*******'
+export const DIALOGFLOW_CLIENT_EMAIL = '*******'
+export const DIALOGFLOW_PROJECT_ID = '*******'
+export const OPENWEATHERAPPID = '*******'
+export const open_weather_base = 'https://api.openweathermap.org/data/2.5/weather'
+export const COVID_DATA='https://www.mohfw.gov.in/data/datanew.json'
